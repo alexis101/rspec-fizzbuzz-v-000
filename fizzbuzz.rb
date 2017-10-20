0cde462b848300e25a307d65532ca4a5b77086c7
alexis101 <kondilisalexis@gmail.com>
2017-10-20 16:19:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(int)
     "Fizz"
 elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 or 5 == 15 or 0
+  elsif int % 3 == 15 or int % 5 == 15
     "FizzBuzz"
   else !(int % 3 && 5 == 15 or 0)
     "nil"
